e89a8e36bb27993372800a23ac90f7c9be04b743
brainmentorspvtltd <kajal1231995@gmail.com>
2022-10-20 21:15:47 +0530

    array

--- Array.java
+++ Array.java
@@ -2,8 +2,33 @@ import java.util.Scanner;
 
 public class Array {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] student = new int[]{10,20, 30, 40, 50 , 60};//initialization
+
+
+//2 -D arrays
+        int [][] arr1 =new int[3][5];//
+
+
+
+        int [][]arr = new int[4][];
+
+        arr[0]= new int[2];
+        arr[1] =new int[4];
+        arr[2] =new int[1];
+        arr[3] =new int[4];
+
+        // int [] studentarr =new int[5];
+        // int studentarr2 [] =new int[5];
+        // int[] studentarr3, studentarr4;
+        // studentarr3 =new int[8];
+        // studentarr4= new int[]{1 ,2 ,3 ,4};
+        // int student5[], student6;
+        // student5 =new int[4];
+        // student6=34;
+
+
+        //1 -D array 
+        // Scanner sc = new Scanner(System.in);
+        // int[] student = new int[]{10,20, 30, 40, 50 , 60};//initialization
         ;//int [] // new keyword is used to create an object 
 
         //base Addd + index*data_type_size
@@ -13,9 +38,9 @@ public class Array {
         // }
         //printing
         //student.length = auto detects the length of array int[5] length =5
-        for(int i=0;i< student.length;i++){
-            System.out.print(student[i]+" ");
-        }
+        // for(int i=0;i< student.length;i++){
+        //     System.out.print(student[i]+" ");
+        // }
 
         //int student1 =20;// int 
         // int student2 =14 ;
