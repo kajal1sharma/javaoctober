953413314432b31c2721caecc10ca93b72f737c1
brainmentorspvtltd <kajal1231995@gmail.com>
2022-10-20 20:40:17 +0530

    array declaration

--- Array.java
+++ Array.java
@@ -3,14 +3,17 @@ import java.util.Scanner;
 public class Array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] student = new int[10];//int [] // new keyword is used to create an object 
-//base Addd + index*data_type_size
-        //input
-        for(int i=0;i< 10 ;i++){
-            student[i] = sc.nextInt();
-        }
+        int[] student = new int[]{10,20, 30, 40, 50 , 60};//initialization
+        ;//int [] // new keyword is used to create an object 
+
+        //base Addd + index*data_type_size
+        // //input
+        // for(int i=0;i< 10 ;i++){
+        //     student[i] = sc.nextInt();
+        // }
         //printing
-        for(int i=0;i< 10 ;i++){
+        //student.length = auto detects the length of array int[5] length =5
+        for(int i=0;i< student.length;i++){
             System.out.print(student[i]+" ");
         }
 
