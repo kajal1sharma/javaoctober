f0b951371d2dc435c197f2ab5c8ddcc5ab7fb209
brainmentorspvtltd <kajal1231995@gmail.com>
2022-11-21 20:48:21 +0530

    abstract class

---
+++ Abstract.java
@@ -0,0 +1,82 @@
+import java.sql.PseudoColumnUsage;
+
+abstract class Person{
+//cannit create object of this class
+    String name;
+    int  age;
+    int height;
+    int weight;
+
+    abstract void dressCode();
+    void run(){
+       //kjkjkjkj 
+    }
+     Person(){
+        //kjkjkjk
+        name="priya";
+     }
+     Person(String name, int age){
+        //kjkjkjk
+        this();
+        name="priya";
+     }
+}
+
+class Teacher extends Person{
+    int salaray;
+    Teacher(){
+        super("riya", 12);
+        salaray=900;
+    }
+    void dressCode(){
+       
+        //kgjdskgdfk,
+    }
+    //abstract void salary();
+
+}
+
+class Student extends Person{
+    int marks;
+    void dressCode(){
+        //kgjdskgdfk,
+    }
+}
+class Engineer extends Person{
+    int salaray;
+    void dressCode(){
+        //kgjdskgdfk,
+    }
+}
+abstract class Doctor extends Person{
+    int salaray;
+    String specialist;
+    //dressCode();
+    // void dressCode(){
+    //     //kgjdskgdfk,
+    // }
+}
+
+class Gynos extends Doctor{
+void dressCode(){
+
+}
+
+}
+
+class Physician extends Doctor{
+    @Override
+    void dressCode() {
+        // TODO Auto-generated method stub
+        
+    }
+}
+
+
+public class Abstract {
+    //abstract 
+
+    public static void main(String[] args) {
+        Teacher t=new Teacher();        
+    }
+}
