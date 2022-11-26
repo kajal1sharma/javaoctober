19ee23ac13f0a903ca43e3021825525435858452
brainmentorspvtltd <kajal1231995@gmail.com>
2022-11-26 21:00:53 +0530

    exception

--- ExceptionTest.java
+++ ExceptionTest.java
@@ -1,25 +1,58 @@
-import java.util.Scanner;
+class ExceptionTest{
 
-public class ExceptionTest {
-    public static void main(String[] args) {
-       //exception occur only at runtime
+    static void doSomething(){
+        System.out.println("this is line 2");
+        doSomethingMore();
+    }
 
-       try{
-       System.out.println("enter a number");
-        Scanner sc =new Scanner(System.in);
-        int a =sc.nextInt();
-
-        int arr[]= new int[10];
-        System.out.println(arr[12]);
-        //file abc => "the "
-       }catch(Exception e){
-        //file handling
-        //file cdf => "the "
+    static void doSomethingMore(){
+        System.out.println("this is line 3");
+        try{
+            int a=90/0;
+        }
+        catch(Exception e){
+            int a=90/8;
+        }
         
-        System.out.println(e);
-           // System.out.print("some exception has arised");
-       }
+            doEvenMore();
+    }
 
+    static void doEvenMore(){
+        System.out.println("this is line 4");
+    }
 
+    public static void main(String[] args) {
+        System.out.println("this is line 1");
+        doSomething();
     }
 }
+
+
+
+
+
+// import java.util.Scanner;
+
+// public class ExceptionTest {
+//     public static void main(String[] args) {
+//        //exception occur only at runtime
+
+//        try{
+//        System.out.println("enter a number");
+//         Scanner sc =new Scanner(System.in);
+//         int a =sc.nextInt();
+
+//         int arr[]= new int[10];
+//         System.out.println(arr[12]);
+//         //file abc => "the "
+//        }catch(Exception e){
+//         //file handling
+//         //file cdf => "the "
+        
+//         System.out.println(e);
+//            // System.out.print("some exception has arised");
+//        }
+
+
+//     }
+// }
